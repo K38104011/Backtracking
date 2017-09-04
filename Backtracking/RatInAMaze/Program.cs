820932363b10bea875bb4b128771d3a3c61aea64
Phạm Đặng Hoàng Giang <phamdanghoanggiang@gmail.com>
2017-09-04 20:11:03 +0700

    Rat in a maze problem

---
+++ Backtracking/RatInAMaze/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RatInAMaze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rm = new RatMaze(new [,]
+            {
+                {1, 0, 0, 0},
+                {1, 1, 1, 1},
+                {0, 1, 0, 0},
+                {1, 1, 1, 1}
+            });
+            rm.PrintResult();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Backtracking/RatInAMaze/RatMaze.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RatInAMaze
+{
+    public class RatMaze
+    {
+        class Position
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+        private readonly int[,] _maze;
+        private readonly Position[] _steps = 
+        {
+            new Position
+            {
+                X = 0,
+                Y = 1
+            },
+            new Position
+            {
+                X = 1,
+                Y = 0
+            }
+        };
+
+        public RatMaze(int[,] maze)
+        {
+            _maze = (int[,]) maze.Clone();
+            for (var i = 0; i < maze.GetLength(0); i++)
+            {
+                for (var j = 0; j < maze.GetLength(1); j++)
+                {
+                    Console.Write(maze[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            _maze[0, 0] = 2;
+            BuildResult(new Position {X = 0, Y = 0});
+        }
+
+        private bool BuildResult(Position currentPosition)
+        {
+            if (currentPosition.X == _maze.GetLength(0) - 1 && currentPosition.Y == _maze.GetLength(1) - 1)
+            {
+                return true;
+            }
+            foreach (var step in _steps)
+            {
+                var nextPositionX = currentPosition.X + step.X;
+                var nextPositionY = currentPosition.Y + step.Y;
+                if (nextPositionX < 0 || nextPositionY < 0 || nextPositionX > _maze.GetLength(0) - 1 ||
+                    nextPositionY > _maze.GetLength(1) - 1 || _maze[nextPositionX, nextPositionY] == 0) continue;
+                _maze[nextPositionX, nextPositionY] = 2;
+                if (BuildResult(new Position { X = nextPositionX, Y = nextPositionY }))
+                {
+                    return true;
+                }
+                _maze[nextPositionX, nextPositionY] = 1;
+            }
+            return false;
+        }
+
+        public void PrintResult()
+        {
+            for (var i = 0; i < _maze.GetLength(0); i++)
+            {
+                for (var j = 0; j < _maze.GetLength(1); j++)
+                {
+                    Console.Write(_maze[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
